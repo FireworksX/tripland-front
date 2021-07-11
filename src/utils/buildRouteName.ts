6c866504b98a4862824129269f37296a00522eb2
arturabeltins <abeltinsh.a@icloud.com>
2021-07-11 15:19:40 +0300

    revert old path alias

--- src/utils/buildRouteName.ts
+++ src/utils/buildRouteName.ts
@@ -1,3 +1,3 @@
-import { ROUTE_NAMES } from '~server/router/constants'
+import { ROUTE_NAMES } from '../../server/router/constants'
 
 export const buildRouteName = (...names: string[]): string => [ROUTE_NAMES.rootRoute, ...names].join('.')
